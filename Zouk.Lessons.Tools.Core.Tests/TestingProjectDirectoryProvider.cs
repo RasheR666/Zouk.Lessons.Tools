d85fa313bfc2f5c176063e67e409bb3e97003c1d
RasheR666 <rasher666@gmail.com>
2019-02-24 04:08:04 +0500

    TestingProjectDirectoryProvider

---
+++ Zouk.Lessons.Tools.Core.Tests/TestingProjectDirectoryProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using Testing.Tools.Directory;
+
+namespace Zouk.Lessons.Tools.Core.Tests
+{
+	public static class TestingProjectDirectoryProvider
+	{
+		public static DirectoryInfo SolutionDirectory { get; } = SolutionDirectoryProvider.Get("Zouk.Lessons.Tools.sln");
+		public static DirectoryInfo ZoukLessonsToolsCoreTestsDirectory { get; } = SolutionDirectory.GetOrCreateSubDirectory("Zouk.Lessons.Tools.Core.Tests");
+	}
+}
