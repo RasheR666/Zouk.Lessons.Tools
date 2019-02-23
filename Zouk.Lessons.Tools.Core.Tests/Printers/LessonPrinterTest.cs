c90f7559a0c908b4630f5ecc48abdcd028fb13fa
RasheR666 <rasher666@gmail.com>
2019-02-24 04:08:58 +0500

    LessonPrinterTest

---
+++ Zouk.Lessons.Tools.Core.Tests/Printers/LessonPrinterTest.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using ApprovalTests;
+using ApprovalTests.Namers;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+using Zouk.Lessons.Tools.Core.Printers;
+using Zouk.Lessons.Tools.Core.Tests.Printers.Writers;
+
+namespace Zouk.Lessons.Tools.Core.Tests.Printers
+{
+	[TestFixture]
+	[UseReporter(typeof(DiffReporter))]
+	[UseApprovalSubdirectory(@"TestData\ApprovalFiles")]
+	public class LessonPrinterTest
+	{
+		[Test]
+		[TestCaseSource(nameof(TestCases))]
+		public void Print(List<string> warmupMovements, List<string> movements, string scenarioName)
+		{
+			using(ApprovalResults.ForScenario(scenarioName))
+			{
+				var printer = new LessonPrinter();
+				var sb = new StringBuilder();
+				printer.Print(warmupMovements, movements, new StringBuilderWriter(sb));
+				Approvals.Verify(sb.ToString());
+			}
+		}
+
+		private static IEnumerable<TestCaseData> TestCases
+		{
+			get
+			{
+				TestCaseData CreateTestCaseData(List<string> warmupMovements, List<string> movements, string scenarioName)
+				{
+					return new TestCaseData(warmupMovements, movements, scenarioName).SetName(scenarioName);
+				}
+
+				string GetCountName(List<string> list)
+				{
+					if(list.Count == 0) return "empty";
+					if(list.Count == 1) return "one";
+					if(list.Count == 3) return "few";
+					return "unknown";
+				}
+
+				var warmupCases = new[]
+				{
+					new List<string>(),
+					new List<string> {"warming up"},
+					new List<string> {"warming up", "big warming up", "good warming up"}
+				};
+
+				var movementCases = new[]
+				{
+					new List<string>(),
+					new List<string> {"movements"},
+					new List<string> {"movement", "cool movement", "crazy movement"}
+				};
+
+				foreach(var warmupCase in warmupCases)
+				{
+					foreach(var movementCase in movementCases)
+					{
+						yield return CreateTestCaseData(warmupCase, movementCase, $"{GetCountName(warmupCase)} warmup and {GetCountName(movementCase)} movements");
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Zouk.Lessons.Tools.Core.Tests/Printers/Writers/StringBuilderWriter.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using Zouk.Lessons.Tools.Core.Printers.Writers;
+
+namespace Zouk.Lessons.Tools.Core.Tests.Printers.Writers
+{
+	public class StringBuilderWriter : IWriter
+	{
+		public StringBuilderWriter(StringBuilder stringBuilder)
+		{
+			this.stringBuilder = stringBuilder;
+		}
+
+		public void Dispose()
+		{
+		}
+
+		public void Write(string text = null)
+		{
+			stringBuilder.Append(text);
+		}
+
+		public void WriteLine(string text = null)
+		{
+			stringBuilder.AppendLine(text);
+		}
+
+		private readonly StringBuilder stringBuilder;
+	}
+}
