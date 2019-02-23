5678bbdf3f4bb8f7493ffe9ee9d6017bf8612cf8
RasheR666 <rasher666@gmail.com>
2019-02-23 11:30:35 +0500

    configure program to generate lesson

---
+++ Zouk.Lessons.Tools/Options/LessonDocumentCreatingOptions.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using CommandLine;
+
+namespace Zouk.Lessons.Tools.Options
+{
+	[Verb("create-lesson-document", HelpText = "Создает документ с необходимыми для занятия движениями")]
+	public class LessonDocumentCreatingOptions
+	{
+		[Option("movements-file", Required = true, HelpText = "Xlsx файл с фигурами для связок")]
+		public FileInfo MovementsFile { get; set; }
+
+		[Option("warmup-movements-file", Required = true, HelpText = "Xlsx файл с фигурами для разминки")]
+		public FileInfo WarmupMovementsFile { get; set; }
+
+		[Option("lesson-number", Required = true, HelpText = "Порядковый номер занятия")]
+		public string LessonNumber { get; set; }
+
+		[Option("lesson-filename", Required = true, HelpText = "Имя выходного файла с фигурами для занятия")]
+		public string LessonFilename { get; set; }
+	}
+}
--- Zouk.Lessons.Tools/Program.cs
+++ Zouk.Lessons.Tools/Program.cs
@@ -1,15 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommandLine;
+using Excel.Document.Reader.Xlsx;
+using Zouk.Lessons.Tools.Core.Extractors;
+using Zouk.Lessons.Tools.Core.Printers;
+using Zouk.Lessons.Tools.Core.Printers.Writers;
+using Zouk.Lessons.Tools.Options;
 
 namespace Zouk.Lessons.Tools
 {
-	class Program
+	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
+			Parser.Default.ParseArguments<LessonDocumentCreatingOptions>(args)
+				.WithParsed(CreateLesson);
+		}
+
+		private static void CreateLesson(LessonDocumentCreatingOptions options)
+		{
+			var excelDocumentReader = new XlsxDocumentReader();
+			var lessonExtractor = new LessonMovementsExtractor();
+
+			var movementsDocument = excelDocumentReader.Read(options.MovementsFile.FullName);
+			var warmupMovementsDocument = excelDocumentReader.Read(options.WarmupMovementsFile.FullName);
+
+			var movements = lessonExtractor.Extract(movementsDocument, options.LessonNumber);
+			var warmupMovements = lessonExtractor.Extract(warmupMovementsDocument, options.LessonNumber);
+
+			using(var writer = new FileWriter(options.LessonFilename))
+			{
+				new LessonPrinter().Print(warmupMovements, movements, writer);
+			}
 		}
 	}
 }
